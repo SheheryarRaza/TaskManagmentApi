f2cf90607a40db234d400c9b8b77806409a27ef1
SheheryarRaza <117226030+SheheryarRaza@users.noreply.github.com>
2025-07-09 15:05:08 +0500

    Services And Repositories

---
+++ TaskManagementApi.Core/Services/TaskItemService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementApi.Core.Entities;
+using TaskManagementApi.Core.Interface.IRepositories;
+using TaskManagementApi.Core.Interface;
+
+namespace TaskManagementApi.Core.Services
+{
+    public class TaskItemService : ITaskItemService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TaskItemService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public Task<TaskItem> CreateTaskAsync(TaskItem taskItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteTaskAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TaskItem>> GetAllTasksAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TaskItem?> GetTaskByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateTaskAsync(TaskItem taskItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TaskManagementApi.Infrastructure/Repositories/TaskItemRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagementApi.Core.Data;
+using TaskManagementApi.Core.Entities;
+using TaskManagementApi.Core.Interface.IRepositories;
+
+namespace TaskManagementApi.Core.Repositories
+{
+    public class TaskItemRepository : ITaskItemRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TaskItemRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task AddTaskAsync(TaskItem taskItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteTaskAsync(TaskItem taskItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TaskItem>> GetAllTasksAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TaskItem?> GetTaskByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> TaskItemExistsAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateTaskAsync(TaskItem taskItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
